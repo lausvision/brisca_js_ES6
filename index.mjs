2314273d6e6ddf61e990d1e3981f0b92c970f431
lausvision <lauramolinalopez02@gmail.com>
2020-04-02 14:00:59 +0200

    input data

--- index.mjs
+++ index.mjs
@@ -1,1 +1,39 @@
-import 
+
+
+
+
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+
+// Set input character encoding.
+standard_input.setEncoding('utf-8');
+
+// Prompt user to input data in console.
+console.log("¡Hola! Este es el juego de la Brisca :)");
+console.log("¿cuantos jugadores vais a ser? ¿dos o cuatro?");
+console.log("Si quieres pausar el juego escribe: exit.");
+
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+
+    // User input exit.
+    if(data === 'exit\n'){
+        // Program exit.
+        console.log("User input complete, program exit.");
+        process.exit();
+    }else if (data==='dos\n' || data=== 'cuatro\n' || data==='2\n' || data=== '4\n')
+    {
+        // Print user input in console.
+        console.log('Tenemos : ' + data + ' jugadores   ^-^ ');
+        EstadoBrisca(data);
+
+    }else{
+        console.log('numero de jugadores no valido');
+    }
+});
+
+function EstadoBrisca(playersnum){
+
+    console.log('Iniciamos juego...');
+
+}
